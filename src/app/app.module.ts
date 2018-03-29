640932c2aed2debaf55dc0e20a056b7dd0bedcab
Vedat Akses <vedatakses@gmail.com>
2018-03-30 00:22:19 +0300

    Routing between forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
 
 
 @NgModule({
@@ -17,7 +17,12 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent},
+      { path: 'login', component: LoginComponent},
+      { path: 'signup', component: SignupFormComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
